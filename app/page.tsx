5c08d3fdf50049333377a7a21fb1bd2149a6fe0e
Samir Singh <samirsingh2000.ss@gmail.com>
2024-05-09 17:14:30 +0530

    42:00 time

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import Live from "@/components/Live";
+
 export default function Page() {
   return (
     <div className="h-[100vh] w-full flex justify-center items-center text-center">
       <h1 className="text-5xl text-white">Liveblock Figma clone!</h1>
+
+      <Live />
     </div>
   );
 }
